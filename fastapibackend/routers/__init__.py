5036475f456af1199204c5373d87fbdd6923db16
luxkatana <taseen.bibi@gmail.com>
2026-02-26 23:44:39 +0100

    Verification half finish
    In deze commit heb ik een verificatie systeem gemaakt voor ALLEEN ID
    KAARTEN, voor rijbewijs moet nog gedaan worden, wordt vast OCR maar
    voorlopig wil ik daarop niet focussen.

---
+++ fastapibackend/routers/__init__.py
@@ -0,0 +1,11 @@
+from .authentication_router import (
+    AuthenticationRouter,
+    get_current_user_auth,
+    get_current_user_data,
+)
+from .verification_router import VerificationRouter
+
+VerificationRouter
+AuthenticationRouter
+get_current_user_data
+get_current_user_auth
