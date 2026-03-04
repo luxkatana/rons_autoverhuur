37bdbdbb1d29e00d3730b3d0e476d159ea4dd6f4
luxkatana <taseen.bibi@gmail.com>
2026-03-04 17:20:33 +0100

    Vos autoverhuur support added
    In deze commit heb ik er voor gezorgd dat vos autoverhuur ook zijn
    collectie hier te zien is

--- fastapibackend/db.py
+++ fastapibackend/db.py
@@ -1,5 +1,6 @@
 import pymongo
 import dotenv
+import warnings
 from os import environ
 
 dotenv.load_dotenv()
@@ -12,3 +13,10 @@ database = dbclient.get_default_database()
 cars = database.get_collection("autos")
 authentication = database.get_collection("authentication")
 usersdata = database.get_collection("usersdata")
+db_vosverhuur_client = pymongo.AsyncMongoClient(environ["VOS_AUTOVERHUUR_URI"])
+
+vosautos = db_vosverhuur_client.get_default_database().get_collection("statictestdata")
+if vosautos.name == "statictestdata":
+    warnings.warn(
+        "WARNING: still using testing statictestdata, change it ASAP in db.py and .env file"
+    )
--- fastapibackend/routers/cars_router.py
+++ fastapibackend/routers/cars_router.py
@@ -1,7 +1,7 @@
 import strawberry
 from typing import Optional
 import pydantic
-from db import cars as carsDB
+from db import vosautos, cars as carsDB
 from pydantic_mongo import ObjectIdField
 from strawberry.fastapi import GraphQLRouter
 
@@ -10,6 +10,23 @@ from strawberry.fastapi import GraphQLRouter
 class Pagination:
     items_amount: Optional[int] = 2
     current_page: int
+
+
+class VosCarDatabase(pydantic.BaseModel):
+    id: ObjectIdField = pydantic.Field(alias="_id")
+    age: int
+    brand: str
+    class_: str = pydantic.Field(alias="class")
+    model: str
+    roofbox_option: bool
+    seats: int
+    towbar: bool
+    type: str = pydantic.Field(alias="type")
+    winter_tires: bool
+
+
+#    available: bool
+# TODO: Implement available flag
 
 
 class DatabaseCar(pydantic.BaseModel):
@@ -54,6 +71,51 @@ async def get_cars(pagination: Optional[Pagination] = None) -> list[Car]:
     return cars
 
 
+async def get_vos_cars(pagination: Optional[Pagination] = None):
+    cars = []
+    if pagination is None:
+        async with vosautos.find() as cursor:
+            async for car in cursor:
+                cars.append(VosCar.from_pydantic(VosCarDatabase.model_validate(car)))
+    else:
+        page = pagination.current_page
+        pageSize = pagination.items_amount
+        async with await vosautos.aggregate(
+            [
+                {
+                    "$facet": {
+                        "data": [
+                            {"$skip": (page - 1) * pageSize},
+                            {"$limit": pageSize},
+                        ],
+                    },
+                },
+            ]
+        ) as cursor:
+            async for car in cursor:
+                for car in car["data"]:
+                    cars.append(
+                        VosCar.from_pydantic(VosCarDatabase.model_validate(car))
+                    )
+    return cars
+
+
+@strawberry.experimental.pydantic.type(model=VosCarDatabase)
+class VosCar:
+    id: strawberry.ID
+    age: int
+    brand: str
+    class_: str = strawberry.field(name="class")
+    model: str
+    roofbox_option: bool
+    seats: int
+    towbar: bool
+    type: str
+    winter_tires: bool
+    # available: bool
+    # TODO: Available flag implement
+
+
 @strawberry.experimental.pydantic.type(model=DatabaseCar)
 class Car:
     id: strawberry.ID
@@ -73,7 +135,8 @@ class Car:
 @strawberry.type
 class Query:
     cars: list[Car] = strawberry.field(resolver=get_cars)
+    voscars: list[VosCar] = strawberry.field(resolver=get_vos_cars)
 
 
 schema = strawberry.Schema(Query)
-CarsRouter = GraphQLRouter(schema, "/cars-graphql", "apollo-sandbox")
+CarsRouter = GraphQLRouter(schema, "/cars-graphql", "apollo-sandbox", prefix="/cars")
